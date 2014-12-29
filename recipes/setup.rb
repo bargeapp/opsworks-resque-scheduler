637ff957ca457bb243264a12822730ab3e2d3201
Noah Blumenthal <noah@hackerhasid.com>
2014-12-29 15:36:33 -0500

    read rack_env from deploy, fallback to app setting rack_env or rails_env

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -7,9 +7,11 @@
 # All rights reserved - Do Not Redistribute
 #
 node[:deploy].each do |application, deploy|
+  settings = ((node[:resque] || {})[application] || {})
+  rack_env = deploy[:rails_env] || settings[:rack_env] || settings[:rails_env] 
   template "/etc/init/resque-scheduler-#{application}.conf" do
     source "resque-scheduler.conf.erb"
     mode '0644'
-    variables application: application, deploy: deploy
+    variables application: application, deploy: deploy, rack_env: rack_env
   end
 end
